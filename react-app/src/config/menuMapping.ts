81604c4dba8afc8746ad5f03b7bd4cc1cb977ad1
trevor@3arch.io <trevor@3arch.io>
2025-02-02 09:17:08 -0800

    refactor: remove React application in favor of Flask templates

--- react-app/src/config/menuMapping.ts
+++
@@ -1,9 +0,0 @@
-// Map route keys to menu items
-export const routeToMenuItem: Record<string, string> = {
-  '/dashboard': 'dashboard',
-  '/meetings/all': 'meetings/all',
-  '/meetings/recent': 'meetings/recent',
-  '/meetings/favorites': 'meetings/favorites',
-  '/recording/new': 'recording/new',
-  '/recording/devices': 'recording/devices',
-};
